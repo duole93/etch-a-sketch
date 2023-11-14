5c0361e419a5742c331a788dfb3631a7e39ac59c
Duong Le <duole93@live.com>
2023-11-14 09:34:26 -0600

    add resizable dimenstion, colorpicker, eraser
    - add an input slider to change the drawing pad dimension
    - add an esaser button erase uncessary stroke.
    - add an input color to change the color of the stroke.
    - code refactor

--- js/script.js
+++ js/script.js
@@ -1,56 +1,121 @@
 const gridContainer = document.querySelector(".container-grid");
+const colorPicker=document.querySelector(".btn-color");
 const btnClear = document.querySelector(".btn-clear");
+const btnErase = document.querySelector(".btn-erase");
+const btnBrush = document.querySelector(".btn-brush");
+const btnDimension = document.querySelector("#dimension");
+const txtDimension = document.querySelector(".dimension-text");
+const TOTAL_WIDTH = 400;
 
 //grid size initialize method
 let x = 40;
-let totalWidth = 400;
-let cellWidth = totalWidth / x;
+let grid = createGrid(x);
 let isMouseDown = false;
+let isErase = false;
+let color = '#000000'
 
-//create grid
-gridContainer.style.width = `${totalWidth + 2}px`;
-gridContainer.style.height = `${totalWidth + 2}px`;
+//create grid container
+gridContainer.style.width = `${TOTAL_WIDTH + 2}px`;
+gridContainer.style.height = `${TOTAL_WIDTH + 2}px`;
 gridContainer.style.border = "1px solid gray";
-const grid = Array.from({ length: x * x }, (item, index) => {
-	let cell = document.createElement("div");
-	cell.style.width = `${cellWidth}px`;
-	cell.style.height = `${cellWidth}px`;
-	cell.style.border = "0.1px solid gray";
-	return cell;
-});
 gridContainer.append(...grid);
+toggleEraseBrush(isErase);
+//setColor
+colorPicker.addEventListener('change', (event)=>{
+	color=event.target.value;
+})
 
 //change color when mouse down and over
-document.addEventListener('mousedown', ()=>{
-    isMouseDown=true;
-    console.log('isMouseDown : ', isMouseDown);
+//mouse down is true
+document.addEventListener("mousedown", () => {
+	isMouseDown = true;
 });
 
-document.addEventListener('mouseup',()=>{
-    isMouseDown=false;
-    console.log('isMouseDown : ', isMouseDown);
+//mouse up is true
+document.addEventListener("mouseup", () => {
+	isMouseDown = false;
 });
 
-grid.forEach((item) => {
-	item.addEventListener("mouseenter", (event) => {
-		if (isMouseDown) {
-			console.log(event.type);
-			event.stopPropagation();
-			event.target.style.backgroundColor = "green";
-		}
-	});
-	item.addEventListener("mousedown", (event) => {
-		isMouseDown = true;
-		console.log(event.type);
-		event.stopPropagation();
-		event.target.style.backgroundColor = "green";
-	});
 
-});
-
-//clear
+//clear = reset the drawing pad
 btnClear.addEventListener("click", () => {
 	grid.map((item) => {
 		item.style.backgroundColor = "white";
 	});
+	isErase=false;
+	toggleEraseBrush(false);
+	
 });
+
+//eraser
+btnErase.addEventListener("click", () => {
+	isErase = true;
+	toggleEraseBrush(isErase);
+
+});
+
+//brush
+btnBrush.addEventListener("click", () => {
+	isErase = false;
+	toggleEraseBrush(isErase);
+
+});
+
+//dimension
+btnDimension.addEventListener("change", (event) => {
+	x = event.target.value;
+	
+	console.log("x :>> ", x);
+	let cellWidth=TOTAL_WIDTH/x;
+	txtDimension.textContent=x;
+	//empty current grid
+	grid.length=0;
+	//create new grid
+	grid = createGrid(x);
+
+	//append to gridcontainer
+	gridContainer.replaceChildren(...grid);
+});
+
+//initialize grid with width = x
+function createGrid(x) {
+	//calculate cellWidth
+	let cellWidth = TOTAL_WIDTH / x;
+	//initialize grid
+	return (arr = Array.from({ length: x * x }, () => {
+		let cell = document.createElement("div");
+		cell.style.width = `${cellWidth}px`;
+		cell.style.height = `${cellWidth}px`;
+		cell.style.border = "0.1px solid gray";
+
+		//change color when mouse is down and enter the grid.
+
+	//event listener	
+	cell.addEventListener("mouseenter", (event) => {
+		if (isMouseDown) {
+			if (!isErase) event.target.style.backgroundColor = color;
+			else event.target.style.backgroundColor = "white";
+		}
+	});
+	cell.addEventListener("mousedown", (event) => {
+		if (!isErase) {
+			isMouseDown = true;
+			event.target.style.backgroundColor = color;
+		} else event.target.style.backgroundColor = "white";
+	});
+
+		return cell;
+	}));
+}
+
+//toggle Easer and Brush
+function toggleEraseBrush(erase){
+	if(erase){
+		btnErase.style.backgroundColor = "green";
+		btnBrush.style.backgroundColor = "white";
+	}
+	else{
+		btnErase.style.backgroundColor = "white";
+		btnBrush.style.backgroundColor = "green";
+	}
+}
